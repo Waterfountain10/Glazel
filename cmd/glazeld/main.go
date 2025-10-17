7d2c194cb5042f6f694874232bc4c3fe336006b7
William Kiem Lafond <william.lafond@mail.mcgill.ca>
2025-10-17 16:39:06 +0800

    live agent discovery via Redis works well, outputs multiple agents status every 5 seconds

--- cmd/glazeld/main.go
+++ cmd/glazeld/main.go
@@ -1,37 +1,22 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-    "time"
+	"context"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/Waterfountain10/glazel/internal/orchestrator"
 )
 
-var (
-    httpAddr = flag.String("http", ":8080", "HTTP listen address")
-)
+var redisAddr = flag.String("redis", "localhost:6379", "Redis address")
 
 func main() {
-    flag.Parse()
-    logger := log.New(os.Stdout, "[glazeld] ", log.LstdFlags|log.Lmicroseconds)
-    logger.Printf("starting orchestrator on %s", *httpAddr)
+	flag.Parse()
+	logger := log.New(os.Stdout, "[glazeld] ", log.LstdFlags|log.Lmicroseconds)
+	logger.Println("starting orchestrator with Redis", *redisAddr)
 
-    mux := http.NewServeMux()
-    mux.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
-        w.WriteHeader(http.StatusOK)
-        _, _ = w.Write([]byte("ok"))
-    })
-
-    srv := &http.Server{
-        Addr:              *httpAddr,
-        Handler:           mux,
-        ReadHeaderTimeout: 5 * time.Second,
-    }
-
-    if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-        logger.Fatalf("server error: %v", err)
-    }
-    fmt.Println("orchestrator stopped")
+	ctx := context.Background()
+	reg := orchestrator.NewRegistry(*redisAddr)
+	reg.Monitor(ctx)
 }
